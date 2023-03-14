4de8b82552fb238d3db463017cd7e1b5d479e356
markakash <akashniki@gmail.com>
2023-03-14 21:35:02 +0100

    psyche: Implement virtual A/B
    Co-authored-by: Demon000 <demonsingur@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,20 @@
 
 DEVICE_PATH := device/xiaomi/psyche
 
+# A/B
+AB_OTA_UPDATER := true
+AB_OTA_PARTITIONS := \
+    boot \
+    dtbo \
+    odm \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor_boot \
+    vendor
+
 # Build Hacks
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_DUP_RULES := true
